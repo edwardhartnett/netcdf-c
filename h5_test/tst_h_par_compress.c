8455d7e396da48862553bf0387e60a304c652f3d
Edward Hartnett <edward.hartnett@noaa.gov>
2024-08-29 07:02:27 -0600

    trying parallel szip

--- h5_test/tst_h_par_compress.c
+++ h5_test/tst_h_par_compress.c
@@ -25,12 +25,11 @@
 /* The following code, when uncommented, adds szip testing for
  * parallel I/O. However, this currently fails. I have a support
  * request in to HDF5 about this. Ed 7/8/20 */
-/* #ifdef HAVE_H5Z_SZIP */
-/* #define NUM_COMPRESS_FILTERS 2 */
-/* #else */
-/* #define NUM_COMPRESS_FILTERS 1 */
-/* #endif /\* HAVE_H5Z_SZIP *\/ */
+#ifdef HAVE_H5Z_SZIP
+#define NUM_COMPRESS_FILTERS 2
+#else
 #define NUM_COMPRESS_FILTERS 1
+#endif /* HAVE_H5Z_SZIP */
 
 int
 main(int argc, char **argv)
